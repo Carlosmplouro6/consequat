1ed92b76ee4adf268eabd21e59944c620d24d692
Carlos Louro <carlosmplouro6@gmail.com>
2020-04-04 20:57:28 +0100

    refractory components

--- src/components/ButtonBay/ButtonBay.js
+++ src/components/ButtonBay/ButtonBay.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Button from "../Button/Button";
 import "./ButtonBay.css";
 
 const ButtonBay = () => {
   const [categories, setCategories] = useState([]);
+  const [activeCategory, setACategory] = useState("");
 
   async function fetchCategories() {
     const categoriesAPI = await fetch(
@@ -18,10 +18,36 @@ const ButtonBay = () => {
     fetchCategories();
   }, []);
 
+  function buttonClicked(category) {
+    setACategory(category);
+    console.log(activeCategory);
+    var Buttons = document.getElementsByClassName("categorieButton");
+    for (var i = 0; i < Buttons.length; i++) {
+      if (Buttons[i].id === category) {
+        clearActive();
+        Buttons[i].className += " active";
+      }
+    }
+  }
+
+  function clearActive() {
+    var current = document.getElementsByClassName("active");
+    if (current.length > 0) {
+      current[0].className = current[0].className.replace(" active", "");
+    }
+  }
+
   return (
     <div className="buttonGrid">
-      {categories.map((categories, index) => (
-        <Button key={index} categories={categories} />
+      {categories.map((categories) => (
+        <button
+          key={categories.strCategory}
+          id={categories.strCategory}
+          className="categorieButton"
+          onClick={() => buttonClicked(categories.strCategory)}
+        >
+          {categories.strCategory}
+        </button>
       ))}
     </div>
   );
