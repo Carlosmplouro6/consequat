87b4e82447599f5f4a019f658dcddb1103f9b0a2
Carlos Louro <carlosmplouro6@gmail.com>
2020-04-07 03:22:47 +0100

    Fixed Ingredients List

--- src/components/Recepie/Recepie.js
+++ src/components/Recepie/Recepie.js
@@ -6,6 +6,9 @@ function Recepie() {
   const [category, setCategory, recepie, setRecepie] = useContext(FoodContext);
   const [recepieData, setRecepieData] = useState();
   const [showRecepie, setShowRecepie] = useState(false);
+  const [ingredients, setIngredients] = useState([]);
+  const [measures, setMeasures] = useState([]);
+
   useEffect(() => {
     if (recepie !== "None") {
       fetchRecepie();
@@ -17,10 +20,32 @@ function Recepie() {
       "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + recepie
     );
     const recepieJson = await recepieAPI.json();
+
+    for (let i = 1; i < 21; i++) {
+      if (
+        !recepieJson.meals[0]["strIngredient" + i] == "" ||
+        !recepieJson.meals[0]["strIngredient" + i] == null
+      ) {
+        setIngredients((oldArray) => [
+          ...oldArray,
+          recepieJson.meals[0]["strIngredient" + i],
+        ]);
+        setMeasures((oldArray) => [
+          ...oldArray,
+          recepieJson.meals[0]["strMeasure" + i],
+        ]);
+      }
+    }
     console.log(recepieJson);
     setRecepieData(recepieJson);
     setShowRecepie(true);
     setRecepie("None");
+  }
+
+  function resetPage() {
+    setShowRecepie(false);
+    setIngredients([]);
+    setMeasures([]);
   }
 
   return (
@@ -39,93 +64,20 @@ function Recepie() {
                   />
                   <h4>Ingredients:</h4>
                   <ul>
-                    <li>
-                      {recepieData.meals[0].strIngredient1 + " "}
-                      {recepieData.meals[0].strMeasure1}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient2 + " "}
-                      {recepieData.meals[0].strMeasure2}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient3 + " "}
-                      {recepieData.meals[0].strMeasure3}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient4 + " "}
-                      {recepieData.meals[0].strMeasure4}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient5 + " "}
-                      {recepieData.meals[0].strMeasure5}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient6 + " "}
-                      {recepieData.meals[0].strMeasure6}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient7 + " "}
-                      {recepieData.meals[0].strMeasure7}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient8 + " "}
-                      {recepieData.meals[0].strMeasure8}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient9 + " "}
-                      {recepieData.meals[0].strMeasure9}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient10 + " "}
-                      {recepieData.meals[0].strMeasure10}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient11 + " "}
-                      {recepieData.meals[0].strMeasure11}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient12 + " "}
-                      {recepieData.meals[0].strMeasure12}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient13 + " "}
-                      {recepieData.meals[0].strMeasure13}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient14 + " "}
-                      {recepieData.meals[0].strMeasure14}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient15 + " "}
-                      {recepieData.meals[0].strMeasure15}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient16 + " "}
-                      {recepieData.meals[0].strMeasure16}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient17 + " "}
-                      {recepieData.meals[0].strMeasure17}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient18 + " "}
-                      {recepieData.meals[0].strMeasure18}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient19 + " "}
-                      {recepieData.meals[0].strMeasure19}
-                    </li>
-                    <li>
-                      {recepieData.meals[0].strIngredient20 + " "}
-                      {recepieData.meals[0].strMeasure20}
-                    </li>
+                    {ingredients.map((ingredients, index) => (
+                      <li key={index}>
+                        {ingredients} - {measures[index]}
+                      </li>
+                    ))}
                   </ul>
                   <p className="recepieInstructions">
                     {recepieData.meals[0].strInstructions}
                   </p>
                   <button
                     className="CloseBtn"
-                    onClick={() => setShowRecepie(false)}
+                    onClick={() => {
+                      resetPage();
+                    }}
                   >
                     X
                   </button>
