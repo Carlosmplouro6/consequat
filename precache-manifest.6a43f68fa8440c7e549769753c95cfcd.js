660cb7ddb7b1522964da4479e3cd32ba3139e048
Carlos Louro <carlosmplouro6@gmail.com>
2020-04-08 02:03:15 +0100

    Updates

---
+++ precache-manifest.6a43f68fa8440c7e549769753c95cfcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44719ab7224282471b0cd2432e12d45f",
+    "url": "/consequat/index.html"
+  },
+  {
+    "revision": "a08725837bb8dd716893",
+    "url": "/consequat/static/css/main.1d7962a3.chunk.css"
+  },
+  {
+    "revision": "25fe4c62e462c06dd6e0",
+    "url": "/consequat/static/js/2.f54fe4fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/consequat/static/js/2.f54fe4fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08725837bb8dd716893",
+    "url": "/consequat/static/js/main.d1fb0892.chunk.js"
+  },
+  {
+    "revision": "29916ec94993c7d3241e",
+    "url": "/consequat/static/js/runtime-main.32a7431d.js"
+  }
+]);
